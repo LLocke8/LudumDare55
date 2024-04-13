c80be652fa3ea6ebbc89d26b20c30f0e78acb6f1
David <pizzalol12356@gmail.com>
2024-04-13 15:47:36 +0200

    Menu 1.0
    Menu completo con opzioni, Volume , Possibilità di cambiare modalità di finestra, start game, exit game.

---
+++ Main_menu/main_menu.gd
@@ -0,0 +1,38 @@
+class_name Main
+
+extends Control
+
+@onready var start_game = $MarginContainer/HBoxContainer/VBoxContainer/Start_game as Button
+@onready var options = $MarginContainer/HBoxContainer/VBoxContainer/Options as Button
+@onready var exit_game = $MarginContainer/HBoxContainer/VBoxContainer/Exit_game as Button
+@onready var options_menu = $Options_menu as OptionMenu
+@onready var margin_container = $MarginContainer as MarginContainer
+#Mettere la scena principale 
+#@onready var start_level = preload("")
+
+func _ready():
+	handle_connecting_signals()
+
+	
+func on_start_pressed() -> void:
+	pass
+	#get_tree().change_scene_to_packed(start_level)
+
+func on_options_pressed() -> void:
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+ 
+func on_exit_pressed()-> void: 
+	get_tree().quit()
+
+func on_exit_options_menu() -> void: 
+	margin_container.visible = true
+	options_menu.visible = false
+
+func handle_connecting_signals() -> void:
+	start_game.button_down.connect(on_start_pressed)
+	options.button_down.connect(on_options_pressed)
+	exit_game.button_down.connect(on_exit_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
+
