3b2edbfc6be25cc436c4fb221e078dfd2ef4bfe5
Locke8 <68960827+LLocke8@users.noreply.github.com>
2024-04-13 18:22:51 +0200

    Aggiunto codice per giocatore e nemico
    hp giocatore
    hp nemico
    mana giocatore

--- Sistemi/GameManager.gd
+++ Sistemi/GameManager.gd
@@ -5,6 +5,21 @@ extends Node
 #riferimento al mazzo 
 #var Mazzo : Mazzo = ref
 
+var MaxHPgiocatore : int = 10 :
+	set(Mhp): set_MaxHPgiocatore(Mhp)
+var HPgiocatore : int = 10 : 
+	set(hp): set_hp(hp)
+
+var Maxanime : int = 5 :
+	set(maxa): set_Maxanime(maxa)
+var Anime : int = 5 :
+	set(nanime): set_anime(nanime)
+
+var MaxHPnemico : int = 10 : 
+	set(nmaxhpn) : set_MaxHPnemico(nmaxhpn)
+var HPnemico : int = 10 :
+	set(nhpnem) : set_HPnemico(nhpnem)
+
 var turno : bool = 0 : #0 per giocatore, 1 per nemico
 	set(nturno):
 		if turno:
@@ -38,3 +53,40 @@ func prossima_fase(nfase):
 		#
 		pass
 	fase = nfase 
+
+func set_MaxHPgiocatore(Mhp):
+	MaxHPgiocatore = Mhp 
+	#modifica UI
+
+func set_hp(hp):
+	HPgiocatore = hp 
+	if HPgiocatore <= 0:
+		#vai a scena di game over. 
+		pass
+	elif HPgiocatore > MaxHPgiocatore:
+		HPgiocatore = MaxHPgiocatore
+	#modifica UI
+
+func set_Maxanime(maxa):
+	Maxanime = maxa
+	#modifica UI
+
+func set_anime(nanime):
+	Anime = nanime
+	if Anime > Maxanime:
+		Anime = Maxanime
+	#modifica UI
+
+func set_MaxHPnemico(nmaxhpn):
+	HPnemico = nmaxhpn 
+	if HPnemico <= 0:
+		#vai a scena di vittoria/al prossimo livello. 
+		pass
+	elif HPnemico > MaxHPnemico:
+		HPnemico = MaxHPnemico
+	#modifica UI
+
+func set_HPnemico(nhpnem):
+	HPnemico = nhpnem
+	#modifica UI
+
