e677eacbae23ffa3aff80514582433dbbf78eba0
Colalife67 <146384469+Colalife67@users.noreply.github.com>
2024-04-14 00:34:46 +0200

    Script Carte
    Funzione per mostrare le carte ottenute con le loro statistiche

--- Carte/carta.gd
+++ Carte/carta.gd
@@ -10,9 +10,13 @@ var NomeCarta = 'ScheletroSpada'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(ImgCarta)
+	print(InfoCarta)
 	$Sfondo.texture = load(ImgCarta)
-
+	var attacco = str(InfoCarta[1])
+	var vita = str(InfoCarta[2])
+	var costo = str(InfoCarta[3])
+	$barre/TopBarra/Nome/CenterContainer/nome.text = NomeCarta
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
