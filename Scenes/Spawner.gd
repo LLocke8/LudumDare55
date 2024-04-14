721db542031c03f0c21b0109c6a43e46d676a8f4
Locke8 <68960827+LLocke8@users.noreply.github.com>
2024-04-14 18:26:13 +0200

    Ordinato il codice

--- Scenes/Spawner.gd
+++ Scenes/Spawner.gd
@@ -1,13 +1,25 @@
 extends Node2D
 
-#var enemy_scene = preload("res://Scenes/Enemy.tscn")
-class_name MyClass
 
+class_name Spawner
+
+var Waves = [
+	{
+	"Sword_Skeleton":[Vector2(10,10),Vector2(10,10),Vector2(10,10)],
+	"Ghost":[Vector2(10,10)],
+	"Shield_Skeleton":[Vector2(10,10)]
+	},
+	{
+		"Archer_Skeleton":[Vector2(10,10)], 
+		"Sword_Skeleton":[Vector2(10,10)],
+		"Ghost":[Vector2(10,10)],
+		"Shield_Skeleton":[Vector2(10,10)]
+	}
+	]
 
 # Called when the node enters the scene tree for the first time.
 
-
-func spawn_wave(enemyspawn:Dictionary): #enemy has  EnemyTipe -> point
+func Spawn_wave(enemyspawn:Dictionary): #enemy has  EnemyTipe -> point
 	var c = 0 # simply counter
 	var enemy_type = enemyspawn.keys() # take all the type  of the enemy and put them to an array
 	#var spawnpoint = enemyspawn.values()# take all the spawnpoint and put them to an array
@@ -17,14 +29,8 @@ func spawn_wave(enemyspawn:Dictionary): #enemy has  EnemyTipe -> point
 		while i < quantity_of_enemy.size(): # how many time the same enemy must be spawned
 			var enemy_scene = load("res://Scenes/Enemy_" + enemy_type[c] + ".tscn") #Load the specific enemy
 			var enemy = enemy_scene.instantiate()# instantiate it
-			var nodes = get_tree().get_nodes_in_group("spawn")#get all the possible spawn position
-			var node = nodes[enemyspawn[c][i]] # get the exactly position where the enemy spawn
-			$SpawnLocation.position = node.position  #target the position
-			enemy.position = $SpawnLocation.position#set the position
+			enemy.position = enemyspawn[c][i] #set the position
 			add_child(enemy) # spawn the enemy_type at the spawnpoint point
 			i= i + 1 # just an increase
 		c= c + 1# just an increase
-
-
-
 
