2981f88e8bf84cf424cec43c284a6d35c57bc3b2
Colalife67 <146384469+Colalife67@users.noreply.github.com>
2024-04-15 16:13:25 +0200

    Carte
    Carte fixate con Grafica ed entita'

--- Carte/carta.gd
+++ Carte/carta.gd
@@ -1,32 +1,11 @@
-extends TextureRect
+extends Node
 
-@onready var CardData = load("res://Carte/CardData.gd")
 
-var nome_carta : String 
-var costo : int = 1
-
-var dm : Node
-
-var is_locked : bool = 0 :
-	set(lock):
-		is_locked = lock
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
 	pass
-	#aggiungere parte grafica
-
-func play_card(pos : Vector2):
-	if !is_locked:
-		dm.card_played(self)
-		card_effect(pos)
-		queue_free()
-
-func _on_button_pressed():
-	if !is_locked:
-		dm.selected(self)
-
-func card_effect(pos : Vector2):
-	var spawn = load(CardData.DATA[nome_carta][6])
-	var inst_spawn = spawn.instantiate()
-	inst_spawn.global_position = pos
-	dm.nodospawn.add_child(inst_spawn)
