ebd04952312f510c841c94af5237a3a3ea1452aa
Locke8 <68960827+LLocke8@users.noreply.github.com>
2024-04-16 18:12:41 +0200

    Bugfix vittoria

--- Carte/CardData.gd
+++ Carte/CardData.gd
@@ -2,7 +2,6 @@ extends Node
 
 #informazioni unita' = [tipo,costo,hp,immagine,descrizione,scena,scena_unita]
 const DATA = {
-
 	'Skeleton swordman' :
 		['',1, 7, "res://Carte/Sprite Carte/Carta Spada.png", "Once in noble battles... nothing, he is just a skeleton, the melee one (he has a sword, of course), what did you expect?","res://Carte/Carta.tscn","res://Entity/Unit/Sword Skeleton/Sword_Skeleton.tscn"],
 	'Skeleton Archer' :
@@ -12,8 +11,7 @@ const DATA = {
 	'Phantom':
 		['',3, 4, "res://Carte/Sprite Carte/Carta Phantom.png",'He has fangs because he thought it was "edgy", still gets one shotted by almost everything.',"res://Carte/carta.tscn","res://Entity/Unit/Phantom/Phantom.tscn"],
 	'Living Armor' :
-		['',7, 50, "res://Carte/Sprite Carte/Carta Bullone.png",'The real deal, the Boss among the army of useless undead, the best tank, the best frontline and the best unti among them, except he takes double the emotional damage because his head is small.
-Do not make fun of his name, he is sensitive about the topic...',"res://Carte/carta.tscn","res://Entity/Unit/Living Wisp/Living_Wisp.tscn"],
+		['',7, 50, "res://Carte/Sprite Carte/Carta Bullone.png",'The real deal, the Boss among the army of useless undead, the best tank, the best frontline and the best unti among them, except he takes double the emotional damage because his head is small. Do not make fun of his name, he is sensitive about the topic...',"res://Carte/carta.tscn","res://Entity/Unit/Living Wisp/Living_Wisp.tscn"],
 	'Sorcerer':
 		['',3, 5, "res://Carte/Sprite Carte/Carta Sorcerer.png",'Meant to be the main character, born to be a stupid minion..., literally.',"res://Carte/carta.tscn","res://Entity/Unit/Mage Skeleton/Mage Skeleton.tscn"],
 	'Heal' :
@@ -23,10 +21,9 @@ Do not make fun of his name, he is sensitive about the topic...',"res://Carte/ca
 	'Tomb' :
 		['',3, 3, "res://Carte/Sprite Carte/Coffin.png", 'lasts until it has hp remaining. Loses 1hp per turn, summon up to 4 skeleton swordman each tun, 1 every 15s',"res://Carte/carta.tscn","res://Entity/Structure/Coffin/Coffin.tscn"],
 	'Wooden Wall' :
-		['',1, 25, "res://Carte/Sprite Carte/wall.png", 'Lasts until destroyed. Nothing particular, blocks enemies and damage, what did you expect.', "res://Carte/carta.tscn","res://Entity/Structure/Wall/Wall.tscn"],
+		['',1, 25, "res://Carte/Sprite Carte/wall.png", 'Lasts until destroyed. Nothing particular, blocks enemies and damage, what did you expect.',"res://Carte/carta.tscn","res://Entity/Structure/Wall/Wall.tscn"],
 	'Soul Turret' :
-		['',2, 10, "res://Carte/Sprite Carte/Soulturret.png", 'Lasts until destroyed. Shoots magical fire as projectiles. Can hit ghosts.', "res://Carte/carta.tscn","res://Entity/Structure/Wisp turret/Wisp turret.tscn"]
-		
+		['',2, 10, "res://Carte/Sprite Carte/Soulturret.png", 'Lasts until destroyed. Shoots magical fire as projectiles. Can hit ghosts.',"res://Carte/carta.tscn","res://Entity/Structure/Wisp turret/Wisp turret.tscn"]
 }
 
 
